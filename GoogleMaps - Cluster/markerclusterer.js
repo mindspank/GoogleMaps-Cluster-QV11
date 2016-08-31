4826102dc8d3f027f321be64d269a160e352d76c
osmar.filho <osmar.filho@dataprev.gov.br>
2016-08-31 17:43:30 -0300

    (osmarcf) invalid SVN repository. Google has changed it to a GitHub one.

--- GoogleMaps - Cluster/markerclusterer.js
+++ GoogleMaps - Cluster/markerclusterer.js
@@ -187,7 +187,7 @@ function MarkerClusterer(map, opt_markers, opt_options) {
  * @private
  */
 MarkerClusterer.prototype.MARKER_CLUSTER_IMAGE_PATH_ =
-    'http://google-maps-utility-library-v3.googlecode.com/svn/trunk/markerclusterer/' +
+    'https://github.com/googlemaps/js-marker-clusterer/tree/gh-pages/' +
     'images/m';
 
 
